7faf2b545dfe0a0d450ceb19c2e4341802d83536
cheeseBreadsticks <bkao28@punahou.edu>
2026-02-25 15:39:25 -1000

    more

--- Scripts/Disasters/Whiteout.java
+++ Scripts/Disasters/Whiteout.java
@@ -1,9 +1,10 @@
 package Disasters;
-public class Blizzard {
+
+public class Whiteout {
     private double distMod; // percentage 1 to -1, -1 is can't move, 1 is double speed, mult with other disasters/events
     private int duration; // blizzard length in days
     private double compoundChance; //chance for other disasters to happen. 0 = none, 1 = normal, 2 = double, etc.
-    public Blizzard() {
+    public Whiteout() {
         this.distMod = 0.2f; 
         this.duration = 6;
         this.compoundChance = 0.5f;
