a590cfd967609a73328cd7d89bcb5a39dff13145
cheeseBreadsticks <bkao28@punahou.edu>
2026-02-25 14:29:12 -1000

    added some start text to Game.java

--- Scripts/game.java
+++ Scripts/game.java
@@ -1,5 +1,9 @@
-public class game {
+public class Game {
   public static void main(String[] args) {
-    System.out.println("")
+    System.out.println("Welcome to the Gobrin Trail!");
+    System.out.println("Your goal is to travel safely across the Gobrin Ice and find freedom in Karhide!");
+    System.out.println("You must manage your resources wisely and make strategic decisions to survive the harsh conditions of the Gobrin Ice.");
+    System.out.println("Along the way, you will encounter various obstacles and disasters");
+    System.out.println("Good luck on the ice!");
   }
 }
