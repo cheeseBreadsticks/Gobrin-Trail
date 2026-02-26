c46027a783ced63aaa7b57c1f4f0dd3072436eca
cheeseBreadsticks <bkao28@punahou.edu>
2026-02-25 16:31:23 -1000

    Changing parts of Game.java

--- Scripts/game.java
+++ Scripts/game.java
@@ -1,9 +1,14 @@
 public class Game {
-  public static void main(String[] args) {
+  public static void start() {
     System.out.println("Welcome to the Gobrin Trail!");
-    System.out.println("Your goal is to travel safely across the Gobrin Ice and find freedom in Karhide!");
+    System.out.println("Your goal is to travel safely across the Gobrin Ice and find freedom in Karhide, 840 miles away!");
     System.out.println("You must manage your resources wisely and make strategic decisions to survive the harsh conditions of the Gobrin Ice.");
     System.out.println("Along the way, you will encounter various obstacles and disasters");
     System.out.println("Good luck on the ice!");
+  }
+
+  public static void main(String[] args) {
+    start();
+    
   }
 }
