ac73102201b70450db8e88861443aa0c516f24f6
nblack28-1 <nblack28@punahou.edu>
2026-02-27 10:57:20 -1000

    bloogity

--- Scripts/Disaster.java
+++ Scripts/Disaster.java
@@ -1,12 +1,21 @@
 public class Disaster {
-    private double weight = 0.05f; // chance for disaster to happen each day, 0-1;
+    private double weight; // chance for disaster to happen each day, 0-1;
     private double distMod; // percentage 1 to -1, -1 is can't move, 1 is double speed, mult with other disasters/events
     private int duration; // blizzard length in days
     private double compoundChance; //chance for other disasters to happen. 0 = none, 1 = normal, 2 = double, etc.
-    public Disaster() {
-        this.weight = 0.05f;
-        this.distMod = 0.2f; 
-        this.duration = 6;
-        this.compoundChance = 0.5f;
+    private boolean active;
+    public Disaster(double w, double dm, int d, double cc) {
+        weight = w;
+        distMod = dm;
+        duration = d;
+        compoundChance = cc;
+        active = false;
+    }
+    public void tick() {
+        active = false;
+        double e = Math.random();
+        if (e < weight) {
+
+        }
     }
 }
